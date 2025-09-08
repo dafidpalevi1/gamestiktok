059c1e0bc0cb9624668294f0d5bc0695501b2869
dafid palevi <43629045+dafidpalevi1@users.noreply.github.com>
2025-09-09 03:03:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,81 +1,129 @@
-const animationContainer = document.getElementById('gift-animation');
-const giftNameEl = document.getElementById('gift-name');
+const giftDisplay = document.getElementById('gift-display');
+const giftImg = document.getElementById('gift-img');
+const giftMessage = document.getElementById('gift-message');
 const giftButtonsContainer = document.getElementById('gift-buttons');
-const audioContainer = document.getElementById('audio-container');
-let currentAnimation = null;
 
-// Data semua gift
+let currentAudio = null;
+
+// Data semua gift, sesuaikan gambar & audio
 const gifts = [
-  {id: 'sayangi-aku', name: 'Sayangi Aku', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/d56945782445b0b8c8658ed44f894c7b~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_g0n4yf.json'},
-  {id: 'mawar', name: 'Mawar ', coins: 1, img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/eba3a9bb85c33e017f3648eaf88d7189~tplv-obj.png', anim: 'https://assets6.lottiefiles.com/packages/lf20_touohxv0.json', audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'},
-  {id: 'donat', name: 'Donat ', coins: 30, img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/4e7ad6bdf0a1d860c538f38026d4e812~tplv-obj.png', anim: 'https://assets6.lottiefiles.com/packages/lf20_jcikwtux.json', audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'},
-  {id: 'hati', name: 'Hati ', coins: 100, img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/6cd022271dc4669d182cad856384870f~tplv-obj.png', anim: 'https://assets1.lottiefiles.com/packages/lf20_puciaact.json', audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'},
-  {id: 'topi', name: 'Topi ', coins: 99, img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/6c2ab2da19249ea570a2ece5e3377f04~tplv-obj.png', anim: 'https://assets6.lottiefiles.com/packages/lf20_vycvjzyp.json', audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3'},
-  {id: 'kumis', name: 'Topi & Kumis ', coins: 99, img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/2f1e4f3f5c728ffbfa35705b480fdc92~tplv-obj.png', anim: 'https://assets10.lottiefiles.com/packages/lf20_5ngs2ksb.json', audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3'},
-  {id: 'tiktok', name: 'TikTok ', coins: 1, img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/802a21ae29f9fae5abe3693de9f874bd~tplv-obj.png', anim: 'https://assets6.lottiefiles.com/private_files/lf30_5ttm3uyv.json', audio: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3'},
-  {id: 'parfum', name: 'Parfum', coins: 10, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/20b8f61246c7b6032777bb81bf4ee055~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_nhjghk.json'},
-  {id: 'kalung', name: 'Kalung Persahabatan', coins: 10, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/e033c3f28632e233bebac1668ff66a2f.png~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_dhv0hj.json'},
-  {id: 'jari-hati', name: 'Jari Hati', coins: 5, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/a4c4dc437fd3a6632aba149769491f49.png~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_g0n4yf.json'},
-  {id: 'menyala', name: 'Menyala', coins: 5, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/fd3dbf6cd817d07798b2e062423994f0.png~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_g0n4yf.json'},
-  {id: 'Music-on-Stage', name: 'Music on Stage', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/d2a59d961490de4c72fed3690e44d1ec.png~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_g0n4yf.json'},
-  {id: 'gg', name: 'GG', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/3f02fa9594bd1495ff4e8aa5ae265eef~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_g0n4yf.json'},
-  {id: 'prajurit-api', name: 'Prajurit Api', coins: 500, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/ac312f0fd7d455561ae2fda251fff836.png~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_iq3rvd.json'},
-  {id: 'rosa', name: 'Rosa', coins: 10, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/eb77ead5c3abb6da6034d3cf6cfeb438~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_iq3rvd.json'},
-  {id: 'tiktok-universe', name: 'Tiktok Universe', coins: 44999, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/8f471afbcebfda3841a6cc515e381f58~tplv-obj.webp', anim: 'https://assets9.lottiefiles.com/private_files/lf30_editor_g0n4yf.json'}
+  {
+    id: 'sayangi-aku',
+    name: 'Sayangi Aku',
+    coins: 1,
+    img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/d56945782445b0b8c8658ed44f894c7b~tplv-obj.webp',
+    audio: 'audio/hay.mp3'
+  },
+  {
+    id: 'mawar',
+    name: 'Mawar',
+    coins: 1,
+    img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/eba3a9bb85c33e017f3648eaf88d7189~tplv-obj.png',
+    audio: 'audio/velocity.mp3'
+  },
+  {
+    id: 'tiktok',
+    name: 'Tiktok',
+    coins: 1,
+    img: 'https://p19-webcast.tiktokcdn.com/img/maliva/webcast-va/802a21ae29f9fae5abe3693de9f874bd~tplv-obj.png',
+    audio: 'audio/hay.mp3'
+  },
+  {
+    id: 'cinta-kamu',
+    name: 'Cinta Kamu',
+    coins: 1,
+    img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/fc549cf1bc61f9c8a1c97ebab68dced7.png~tplv-obj.webp',
+    audio: 'audio/hay.mp3'
+  },
+  {
+    id: 'kopi',
+    name: 'Kopi',
+    coins: 1,
+    img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp',
+    audio: 'audio/hay.mp3'
+  },
+  { id: 'gg', name: 'GG', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/3f02fa9594bd1495ff4e8aa5ae265eef~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'hebat-banget', name: 'Hebat Banget', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/e9cafce8279220ed26016a71076d6a8a.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kerucut-es-krim', name: 'Kerucut Es Krim', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/968820bc85e274713c795a6aef3f7c67~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'jumat-berkah', name: 'Jumat Berkah', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/b6d700a7b8c423443672e14bb3c88b7b.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'es-jeruk', name: 'Es Jeruk', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/7244832db46b7ea5d7d6e280719ddea2~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'sayap-peri', name: 'Sayap Peri', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/e504dc2f313b8c6df9e99a848e1b3a99.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'headphone', name: 'Headphone', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/d250b93197c151c45a08ab5f4db805d6.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kecup-jauh', name: 'Kecup Jauh', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/cdb55940740d5c83879b2934f9a7d08e.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'pelukan-erat', name: 'Pelukan Erat', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/9578adce6e3da2d211583212bdfd1b0e.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'gorengan', name: 'Gorengan', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/003103053dbf1fc8c01a81fcec61a28a.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'go-popular', name: 'Go Popular', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/b342e28d73dac6547e0b3e2ad57f6597.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'sorakan-klub', name: 'Sorakan Klub', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/6a934c90e5533a4145bed7eae66d71bd.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'jari-hati', name: 'Jari Hati', coins: 5, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/a4c4dc437fd3a6632aba149769491f49.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'nasi-padang', name: 'Nasi Padang', coins: 5, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/bd6e927cd68db696f2a27d4e2c7d0c34.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kekuatan-klub', name: 'Kekuatan Klub', coins: 9, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/fb8da877eabca4ae295483f7cdfe7d31.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'rosa', name: 'Rosa', coins: 10, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/eb77ead5c3abb6da6034d3cf6cfeb438~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kalung-persahabatan', name: 'Kalung Persahabatan', coins: 10, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/resource/e033c3f28632e233bebac1668ff66a2f.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'tiket-perjalanan', name: 'Tiket Perjalanan', coins: 10, img: 'https://p16-webcast.tiktokcdn.com/img/alisg/webcast-sg/resource/551ecaf639c5e02354f9e7c1a763ec72.png~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'aku-cinta-kamu', name: 'Aku Cinta Kamu', coins: 10, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/beaaa3a78a00b5b20661b00924ab0e7f~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'parfum', name: 'Parfum', coins: 20, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/20b8f61246c7b6032777bb81bf4ee055~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'Donat', name: 'Donat', coins: 30, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/4e7ad6bdf0a1d860c538f38026d4e812~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-22', name: 'Kopi 22', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-23', name: 'Kopi 23', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-24', name: 'Kopi 24', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-25', name: 'Kopi 25', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-26', name: 'Kopi 26', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-27', name: 'Kopi 27', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-28', name: 'Kopi 28', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-29', name: 'Kopi 29', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+    { id: 'kopi-30', name: 'Kopi 30', coins: 1, img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/02492214b9bd50fee2d69fd0d089c025~tplv-obj.webp', audio: 'audio/hay.mp3' },
+  {
+    id: 'tiktok-universe',
+    name: 'Tiktok Universe',
+    coins: 44999,
+    img: 'https://p16-webcast.tiktokcdn.com/img/maliva/webcast-va/8f471afbcebfda3841a6cc515e381f58~tplv-obj.webp',
+    audio: 'audio/donat.mp3' // bisa ditambah
+  }
 ];
 
-// Generate tombol dan audio
+// Generate tombol gift
 gifts.forEach(gift => {
-  // tombol
   const btn = document.createElement('div');
   btn.className = 'gift-btn';
-  btn.onclick = () => triggerDance(gift.id);
+  btn.onclick = () => showGift(gift.id);
+
   btn.innerHTML = `
     <img src="${gift.img}" alt="${gift.name}">
     <div class="gift-name">${gift.name}</div>
-    <div class="gift-coins"><span>${gift.coins}</span><img src="https://cdn3d.iconscout.com/3d/free/thumb/free-tiktok-coin-3d-icon-png-download-6220601.png"></div>
+    <div class="gift-coins">
+      <img src="https://app.streamtoearn.io/assets/images/coin.png" alt="Coin" class="coin-img">
+      <div class="coin-number">${gift.coins}</div>
+    </div>
   `;
-  giftButtonsContainer.appendChild(btn);
 
-  // audio (jika ada)
-  if (gift.audio) {
-    const audioEl = document.createElement('audio');
-    audioEl.id = 'audio-' + gift.id;
-    audioEl.src = gift.audio;
-    audioContainer.appendChild(audioEl);
-  }
+  giftButtonsContainer.appendChild(btn);
 });
 
-function triggerDance(giftId) {
+
+// Fungsi menampilkan gift
+function showGift(giftId) {
   const gift = gifts.find(g => g.id === giftId);
   if (!gift) return;
 
-  // stop all audio
-  document.querySelectorAll("audio").forEach(a => { a.pause(); a.currentTime = 0; });
-  if (currentAnimation) { currentAnimation.destroy(); currentAnimation = null; }
-  animationContainer.innerHTML = "";
+  // Tampilkan gambar & pesan
+  giftImg.src = gift.img;
+  giftMessage.textContent = `Terima kasih sudah memberikan gift ${gift.name}!`;
+  giftDisplay.classList.remove('hidden');
 
-  giftNameEl.textContent = `${gift.name} (${gift.coins} koin)`;
-  giftNameEl.classList.remove("hidden");
+  // Stop musik sebelumnya
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
 
-  currentAnimation = lottie.loadAnimation({
-    container: animationContainer,
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: gift.anim
-  });
-  animationContainer.classList.remove("hidden");
+  // Play musik gift
+  if (gift.audio) {
+    currentAudio = new Audio(gift.audio);
+    currentAudio.play();
 
-  const audioEl = document.getElementById('audio-' + gift.id);
-  if (audioEl) audioEl.play();
-
-  setTimeout(() => {
-    if (currentAnimation) {
-      currentAnimation.destroy();
-      animationContainer.classList.add("hidden");
-      giftNameEl.classList.add("hidden");
-    }
-    document.querySelectorAll("audio").forEach(a => { a.pause(); a.currentTime = 0; });
-  }, 5000);
+    // Hilangkan gift display otomatis saat musik selesai
+    currentAudio.onended = () => {
+      giftDisplay.classList.add('hidden');
+    };
+  }
 }
